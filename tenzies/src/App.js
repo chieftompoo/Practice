5e9e62ea9690504a12c61df452e76b808f143f2d
Chief Tompoo <thomasyosei@gmail.com>
2022-09-27 17:45:56 -0400

    update the readme

---
+++ tenzies/src/App.js
@@ -0,0 +1,20 @@
+import Dice from "./dice";
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div>
+          <Dice value="1"/>
+          <Dice value="1"/>
+          <Dice value="1"/>
+          <Dice value="1"/>
+          <Dice value="1"/>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ tenzies/src/dice.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dice(props) {
+    return(
+        <div>
+            <h2>{props.value}</h2>
+        </div>
+    )
+}
